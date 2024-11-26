b75f5cc9565665e3975d11e23655c1af95cd4c25
Arttu Paakkinen <Arttu.Paakkinen@hotmail.com>
2024-11-26 12:34:59 +0200

    datagridien muokkausta, varasto sivu tehty

--- Teräksen kirjaus järjestelmä/Sovelluskehitys2024/MainWindow.xaml.cs
+++ Teräksen kirjaus järjestelmä/Sovelluskehitys2024/MainWindow.xaml.cs
@@ -29,9 +29,11 @@ namespace Sovelluskehitys2024
 
             try
             {
-                PaivitaDataGrid("SELECT * FROM tuotteet ORDER BY materiaali", "tuotteet", tuotelista);
+                PaivitaDataGrid("SELECT materiaali, muoto, mitat FROM tuotteet ORDER BY materiaali, muoto", "tuotteet", tuotelista);
                 PaivitaDataGrid("SELECT * FROM asiakkaat", "asiakkaat", asiakaslista);
+                PaivitaDataGrid("SELECT * FROM tuotteet ORDER BY materiaali, muoto", "tuotteet", varastolista);
                 PaivitaAsiakasComboBox();
+                
             }
             catch
             {
@@ -95,6 +97,63 @@ namespace Sovelluskehitys2024
         private void PaivitaAsiakasComboBox()
         {
             //tuotelista_cb.Items.Clear();
+            
+            SqlConnection yhteys = new SqlConnection(polku);
+            yhteys.Open();
+
+            SqlCommand komento = new SqlCommand("SELECT * FROM asiakkaat", yhteys);
+            SqlDataReader lukija = komento.ExecuteReader();
+
+            DataTable taulu = new DataTable();
+            taulu.Columns.Add("ID", typeof(string));
+            taulu.Columns.Add("NIMI", typeof(string));
+
+            while (lukija.Read()) // käsitellään kyselytulos rivi riviltä
+            {
+                int id = lukija.GetInt32(0);
+                string nimi = lukija.GetString(1);
+                taulu.Rows.Add(id, nimi); // lisätään datatauluun rivi tietoineen
+                //tuotelista_cb.Items.Add(lukija.GetString(1));
+            }
+            lukija.Close();
+            yhteys.Close();
+        }
+
+        private void Lisää_tuote_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection yhteys = new SqlConnection(polku);
+            yhteys.Open();
+
+            string kysely = "INSERT INTO tuotteet (materiaali, muoto, mitat, määrä) VALUES ('" + materiaali.Text + "','" + muoto.Text + "','" + mitat.Text + "','" + 0 + "');";
+            SqlCommand komento = new SqlCommand(kysely, yhteys);
+            komento.ExecuteNonQuery();
+
+            yhteys.Close();
+
+            PaivitaDataGrid("SELECT materiaali, muoto, mitat FROM tuotteet ORDER BY materiaali, muoto", "tuotteet", tuotelista);
+            PaivitaDataGrid("SELECT * FROM tuotteet ORDER BY materiaali, muoto", "tuotteet", varastolista);
+        }
+
+
+
+        private void Lisää_Asiakas_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection yhteys = new SqlConnection(polku);
+            yhteys.Open();
+
+            string kysely = "INSERT INTO asiakkaat (yrityksen_nimi, yhteys_henkilö, puhelin_numero) VALUES ('" + yrityksen_nimi.Text + "','" + yhteys_henkilö.Text + "','" + puhelin_numero.Text + "');";
+            SqlCommand komento = new SqlCommand(kysely, yhteys);
+            komento.ExecuteNonQuery();
+
+            yhteys.Close();
+
+            PaivitaDataGrid("SELECT * FROM asiakkaat", "asiakkaat", asiakaslista);
+            PaivitaAsiakasComboBox();
+        }
+
+        private void materiaali_cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //tuotelista_cb.Items.Clear();
 
             SqlConnection yhteys = new SqlConnection(polku);
             yhteys.Open();
@@ -114,78 +173,7 @@ namespace Sovelluskehitys2024
                 //tuotelista_cb.Items.Add(lukija.GetString(1));
             }
             lukija.Close();
-
             yhteys.Close();
         }
-
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            PaivitaDataGrid("SELECT * FROM tuotteet", "tuotteet", tuotelista);
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            SqlConnection yhteys = new SqlConnection(polku);
-            yhteys.Open();
-
-            string kysely = "INSERT INTO tuotteet (materiaali, muoto, mitat) VALUES ('" + materiaali.Text + "','" + muoto.Text + "','" + mitat.Text +"');";
-            SqlCommand komento = new SqlCommand(kysely, yhteys);
-            komento.ExecuteNonQuery();
-
-            yhteys.Close();
-
-            PaivitaDataGrid("SELECT * FROM tuotteet ORDER BY materiaali", "tuotteet", tuotelista);
-        }
-
-        
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            SqlConnection yhteys = new SqlConnection(polku);
-            yhteys.Open();
-
-            string kysely = "INSERT INTO asiakkaat (yrityksen_nimi, yhteys_henkilö, puhelin_numero) VALUES ('" + yrityksen_nimi.Text + "','" + yhteys_henkilö.Text + "','" + puhelin_numero.Text + "');";
-            SqlCommand komento = new SqlCommand(kysely, yhteys);
-            komento.ExecuteNonQuery();
-
-            yhteys.Close();
-
-            PaivitaDataGrid("SELECT * FROM asiakkaat", "asiakkaat", asiakaslista);
-            PaivitaAsiakasComboBox();
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
-        {
-            SqlConnection yhteys = new SqlConnection(polku);
-            yhteys.Open();
-
-
-            yhteys.Close();
-
-        }
-
-        private void toimita_tilaus_Click(object sender, RoutedEventArgs e)
-        {
-            DataRowView rivinakyma = (DataRowView)((Button)e.Source).DataContext;
-            String tilaus_id = rivinakyma[0].ToString();
-            
-            SqlConnection yhteys = new SqlConnection(polku);
-            yhteys.Open();
-
-            string sql = "UPDATE tilaukset SET toimitettu=1 WHERE id='" + tilaus_id + "';";
-
-            SqlCommand komento = new SqlCommand(sql, yhteys);
-            komento.ExecuteNonQuery();
-
-            yhteys.Close();
-        }
-        
     }
 }
--- Teräksen kirjaus järjestelmä/Sovelluskehitys2024/SQLQuery1.sql
+++ Teräksen kirjaus järjestelmä/Sovelluskehitys2024/SQLQuery1.sql
@@ -4,7 +4,11 @@ create table varasto_paikat (id INTEGER IDENTITY(1,1) PRIMARY KEY, varasto_paikk
 
 
 
-drop table asiakkaat;
+--UPDATE tuotteet SET määrä = 0;
+
+--ALTER TABLE tuotteet ADD määrä int;
+
+--drop table asiakkaat;
 
 
 
@@ -33,26 +37,26 @@ drop table asiakkaat;
 
 
 
-CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), hinta INTEGER);
+--CREATE TABLE tuotteet (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), hinta INTEGER);
 
-CREATE TABLE asiakkaat (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), osoite VARCHAR(150), puhelin VARCHAR(50));
+--CREATE TABLE asiakkaat (id INTEGER IDENTITY(1,1) PRIMARY KEY, nimi VARCHAR(50), osoite VARCHAR(150), puhelin VARCHAR(50));
 
-CREATE TABLE tilaukset (id INTEGER IDENTITY(1,1) PRIMARY KEY, asiakas_id INTEGER REFERENCES asiakkaat ON DELETE CASCADE, tuote_id INTEGER REFERENCES tuotteet ON DELETE CASCADE, toimitettu BIT DEFAULT 0);
+--CREATE TABLE tilaukset (id INTEGER IDENTITY(1,1) PRIMARY KEY, asiakas_id INTEGER REFERENCES asiakkaat ON DELETE CASCADE, tuote_id INTEGER REFERENCES tuotteet ON DELETE CASCADE, toimitettu BIT DEFAULT 0);
 
-INSERT INTO tuotteet (nimi, hinta) VALUES ('juusto', 6);
-INSERT INTO asiakkaat (nimi, osoite, puhelin) VALUES ('Masa', 'Kuusikuja 6', '050882682');
-INSERT INTO tilaukset (asiakas_id, tuote_id) VALUES (1,1); 
+--INSERT INTO tuotteet (nimi, hinta) VALUES ('juusto', 6);
+--INSERT INTO asiakkaat (nimi, osoite, puhelin) VALUES ('Masa', 'Kuusikuja 6', '050882682');
+--INSERT INTO tilaukset (asiakas_id, tuote_id) VALUES (1,1); 
 
-DELETE FROM tuotteet WHERE id=5;
+--DELETE FROM tuotteet WHERE id=5;
 
-SELECT * FROM tuotteet;
-SELECT * FROM asiakkaat;
-SELECT * FROM tilaukset;
+--SELECT * FROM tuotteet;
+--SELECT * FROM asiakkaat;
+--SELECT * FROM tilaukset;
 
-UPDATE tilaukset SET toimitettu=1 WHERE id=1
+--UPDATE tilaukset SET toimitettu=1 WHERE id=1
 
-SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id
+--SELECT ti.id as id, a.nimi as asiakas, tu.nimi as tuote FROM tilaukset ti, asiakkaat a, tuotteet tu WHERE a.id=ti.asiakas_id AND tu.id=ti.tuote_id
 
-DELETE FROM tuotteet WHERE nimi="kinkku";
+--DELETE FROM tuotteet WHERE nimi="kinkku";
 
-DROP TABLE tilaukset;
+--DROP TABLE tilaukset;
